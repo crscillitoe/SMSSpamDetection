1072971c4163298472f3c01def1048bb3b6ba8c0
crscillitoe <crscillitoe@wpi.edu>
2019-04-19 09:24:28 -0400

    added neural network code

--- main_neural.py
+++ main_neural.py
@@ -9,7 +9,11 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Conv2D, BatchNormalization
+from keras.utils import to_categorical
+
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from mlxtend.plotting import plot_confusion_matrix
@@ -36,9 +40,9 @@ def main():
         X_pyth.append(entry)
 
         if d['category'] == 'spam':
-            y_pyth.append(0)
+            y_pyth.append([1, 0])
         else:
-            y_pyth.append(1)
+            y_pyth.append([0, 1])
 
     X = np.array(X_pyth)
     y = np.array(y_pyth)
@@ -56,19 +60,53 @@ def main():
     X_test = X[4001:5571]
     y_test = y[4001:5571]
 
-    model = DecisionTreeClassifier()
-    model.fit(X_train, y_train)
+    model = get_neural_network(X.shape[1])
+    model.fit(X_train, y_train, epochs=100, batch_size=512)
 
-    prediction = model.predict(X_test)
-    accuracy = accuracy_score(y_test, prediction)
+    predictions = model.predict(X_test)
+    fitted_predictions = []
 
-    matrix = confusion_matrix(y_test, prediction)
+    # Only classify as spam if we are 90% sure
+    for prediction in predictions:
+        if prediction[0] > 0.9:
+            fitted_predictions.append([1, 0])
+        else:
+            fitted_predictions.append([0, 1])
+
+    fitted_predictions = np.array(fitted_predictions)
+
+
+    accuracy = accuracy_score(y_test, fitted_predictions)
+    print(accuracy)
+
+    matrix = confusion_matrix(y_test.argmax(axis=1), fitted_predictions.argmax(axis=1))
     binary = np.array(matrix)
 
     fig, ax = plot_confusion_matrix(conf_mat=binary)
     plt.show()
 
-    print(accuracy)
+
+def get_neural_network(num_features):
+    model = Sequential()
+
+    # Input Layer
+    model.add(Dense(50, 
+                    activation='relu',
+                    input_shape=(num_features, )))
+
+    # Hidden Layer
+    model.add(Dense(50, 
+                    activation='relu'))
+
+    # Output Layer
+    model.add(Dense(2, kernel_initializer='he_normal'))
+    model.add(Activation('softmax'))
+
+    model.compile(optimizer='sgd',
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+    return model
 
 if __name__ == '__main__':
     main()
