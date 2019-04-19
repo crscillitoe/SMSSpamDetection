3373ff086e15eef67c4dd4bc180a44fb0ef860f0
crscillitoe <crscillitoe@wpi.edu>
2019-04-19 10:24:35 -0400

    api code

---
+++ api/api_neural.py
@@ -0,0 +1,49 @@
+import features
+from data_loader import load_data
+from preprocessing import preprocess_data
+import matplotlib
+
+from api import app
+from api import model
+from flask import jsonify
+from flask import abort
+from flask import request
+from flask_cors import cross_origin
+import keras
+
+# fixes error on mac
+matplotlib.use('TkAgg') 
+
+import numpy as np
+
+@app.route('/api/isSpam', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def is_spam():
+    try:
+        text = request.json["Text"]
+    except:
+        abort(500, 'Text not found')
+
+    X = []
+    entry = (
+        features.currency_count(text),
+        features.url_count(text),
+        features.word_count(text),
+        features.longest_numerical_string(text),
+        features.average_word_length(text),
+        features.num_win_occurences(text),
+        features.num_free_occurences(text)
+    )
+
+    X.append(entry)
+    X = np.array(X)
+
+    prediction = model.predict(X)
+    print(prediction)
+    if (prediction[0])[0] > 0.9:
+        return jsonify({"prediction": "Spam"})
+
+    return jsonify({"prediction": "Not Spam"})
+
+if __name__ == '__main__':
+    main()
---
+++ api/features.py
@@ -0,0 +1,69 @@
+# coding=utf-8
+
+###############################################################
+# features.py - functions that will quantify various features #
+#               for a given data point in the data set        #
+###############################################################
+
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- #
+#     Generic functions, returns vary function to function    #
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- #
+
+
+import re
+
+# Number of times a URL appears in the text
+def url_count(text):
+    # https://www.geeksforgeeks.org/python-check-url-string/
+    # doesn't actually seem to work
+    # url = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    url = '.co|.net|.org'
+    urls = re.findall(url, text.lower())
+    return len(urls)
+
+# Number of times a currency symbol appears in the text
+def currency_count(text):
+    symbol = '\$|£|€|¥|₣'
+    symbols = re.findall(symbol, text)
+    return len(symbols)
+
+def word_count(text):
+    return len(text.split(' '))
+
+# Returns the length of the longest series of consecutive numbers in the given text
+def longest_numerical_string(text):
+    numbers = '\d+'
+    sequences = re.findall(numbers, text)
+    longest = 0
+    for sequence in sequences:
+      if len(sequence) > longest:
+        longest = len(sequence)
+    return longest
+
+# Returns the average word length in the given text
+def average_word_length(text):
+    words = text.split(' ')
+    length = 0
+    for word in words:
+        length += len(word)
+    return length/len(words)
+
+# Number of times the word 'win' occurs in the given text
+def num_win_occurences(text):
+    words = text.split(' ')
+    count = 0
+    for word in words:
+        word = word.lower()
+        if word == 'win' or word == 'winning':
+            count += 1
+    return count
+
+# Number of times the word 'free' occurs in the given text
+def num_free_occurences(text):
+    words = text.split(' ')
+    count = 0
+    for word in words:
+        word = word.lower()
+        if word == 'free':
+            count += 1
+    return count
